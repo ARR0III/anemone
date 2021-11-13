9d8e6449d9d50366673089fba176faf969ba19f1
ARR0III <36902548+ARR0III@users.noreply.github.com>
2021-11-13 23:32:23 +0700

    Add files via upload

--- xtalw.h
+++ xtalw.h
@@ -6,13 +6,21 @@
   affect the operation of the program in any way.
 */
 
+#ifndef XTALW_H
+#define XTALW_H
+
 #include "xtalw.c"
+
+void   strinc(uint8_t * data, int len);
+void   strdec(uint8_t * data, int len);
 
 int    genrand(const int min, const int max);
 int    readfromfile(const char * filename, void * buffer, const size_t length);
 
 size_t printhex(const int tumbler, const void * data, size_t length);
-size_t __strnlen(const char * string, size_t length);
+size_t x_strnlen(const char * string, size_t length);
 
 void * meminit(void * data, const uint8_t simbol, size_t length);
 void * strxor(uint8_t * output, const uint8_t * input, size_t length);
+
+#endif
